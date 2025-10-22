ae541a464db926be2d2a60db3df10c5ac15e46fa
Amid <40906222+ISPZ@users.noreply.github.com>
2025-10-22 11:01:09 +0800

    Add training script for UT-Zappos dataset

--- code/scripts/train_ut.sh
+++ code/scripts/train_ut.sh
@@ -1,1 +1,8 @@
-
+torchrun --nproc_per_node=1 --master_port=29501 train_base_parallel.py \
+    --clip_arch ViT-L/14 \
+    --dataset_path download_data/data/ut-zappos \
+    --save_path out/ut-zappos-adaptive-multi \
+    --yml_path config/ut-zappos.yml \
+    --num_workers 10 \
+    --seed 0 \
+    --same_prim_sample
